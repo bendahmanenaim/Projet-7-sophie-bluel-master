729f43f3b1ed24ec4997cb9bff7f1ad0d143fd49
bendahmane <benaimgm1995@gmail.com>
2024-02-27 13:55:51 +0100

    vérification code modal new photo

--- FrontEnd/modal-new-photo.js
+++ FrontEnd/modal-new-photo.js
@@ -51,44 +51,52 @@ BUTTON_BACK.addEventListener("click",function(){
 BUTTON_ADD.addEventListener("click", function(){
     INPUT_PICTURE.click();
 })
-
-//Selecteur fichier photo
-INPUT_PICTURE.addEventListener("change",function(){
+// Écouteur d'événement pour le sélecteur de fichier photo
+INPUT_PICTURE.addEventListener("change", function(){
+    // Vérifie la taille du fichier et affiche une alerte si elle est trop grande
     if(this.files[0].size > 4194304){
         alert("Fichier trop volumineux");
         this.value = "";
-     };
-     if (this.files[0]) {
+    };
+
+    // Affiche l'aperçu de la photo si un fichier est sélectionné
+    if (this.files[0]) {
         PICTURE_PREVIEW.src = URL.createObjectURL(this.files[0])
-        PICTURE_PREVIEW.style.display="block";
-        PICTURE_SELECTION.style.display="none";
-     }
+        PICTURE_PREVIEW.style.display = "block";
+        PICTURE_SELECTION.style.display = "none";
+    }
 })
 
-//Remise a zero selection image
+// Fonction de remise à zéro de la sélection d'image
 function resetPhotoSelection(){
-    INPUT_PICTURE.value= "";
-    PICTURE_PREVIEW.src ="";
-    PICTURE_PREVIEW.style.display="none"
-    PICTURE_SELECTION.style.display ="block";
+    INPUT_PICTURE.value = "";
+    PICTURE_PREVIEW.src = "";
+    PICTURE_PREVIEW.style.display = "none";
+    PICTURE_SELECTION.style.display = "block";
 }
 
-//Remise a zero formulaire UPLOAD
+// Fonction de remise à zéro du formulaire de téléversement
 function resetForm(){
-    CATEGORIES_SELECT.value= 0;
-    TITLE_NEW_PHOTO.value ="";
+    CATEGORIES_SELECT.value = 0;
+    TITLE_NEW_PHOTO.value = "";
 }
 
-//Chargement CATEGORIES depuis API
+// Fonction de chargement des catégories depuis l'API
 function loadCategories(){
-    CATEGORIES_SELECT.innerHTML = ''; //On vide avant de fetch pour ne pas accumuler les CATEGORIES
+    // Vide le contenu de la liste déroulante avant de récupérer les catégories pour éviter l'accumulation
+    CATEGORIES_SELECT.innerHTML = '';
+    
+    // Crée une option vide pour la catégorie dans le formulaire
     let option = document.createElement("option");
     option.value = 0;
     option.text = "";
-    CATEGORIES_SELECT.add(option);// Ajout CATEGORIE vide dans le formulaire V
+    CATEGORIES_SELECT.add(option);// Ajout de la catégorie vide dans le formulaire
+    
+    // Effectue une requête fetch vers l'API des catégories
     fetch (CATEGORY_API)
-    .then (reponse => reponse.json())
+    .then (response => response.json())
     .then (categories => {
+        // Parcourt les catégories récupérées et les ajoute à la liste déroulante
         for (let category of categories) {
             let option = document.createElement("option");
             option.value = category.id;
@@ -99,16 +107,19 @@ function loadCategories(){
 }
 
 
-//UPLOAD nouveau projet 
+// Fonction de téléversement d'un nouveau projet
 const UPLOAD_WORK = function(){
 
+    // Récupère le jeton d'authentification depuis la session
     let token = sessionStorage.getItem("token");
 
+    // Crée un objet FormData pour envoyer les données du formulaire
     const formData = new FormData();
     formData.append("image", INPUT_PICTURE.files[0]);
     formData.append("title", TITLE_NEW_PHOTO.value);
     formData.append("category", CATEGORIES_SELECT.value);
     
+    // Effectue une requête fetch vers l'API des travaux
     fetch (WORKS_API, {
         method: "POST",
         headers: {
@@ -118,42 +129,54 @@ const UPLOAD_WORK = function(){
         body : formData
     })
     .then (response => {
-        if (response.status===200 ||response.status===201){
-            resetPhotoSelection();//Remise a zero apercu photo 
-            resetForm();//Remise a zero formulaire 
-            refreshWorks(GALLERY_MODALE, true); //Reaffichage travaux dans modale
-            refreshWorks(GALLERY_DIV,false); //Reaffichage traveaux dans index 
+        // Vérifie la réponse du serveur
+        if (response.status === 200 || response.status === 201){
+            // Réinitialise la sélection de la photo
+            resetPhotoSelection();
+            // Réinitialise le formulaire
+            resetForm();
+            // Rafraîchit l'affichage des travaux dans la modal
+            refreshWorks(GALLERY_MODALE, true);
+            // Rafraîchit l'affichage des travaux dans l'index
+            refreshWorks(GALLERY_DIV, false);
+            // Effectue une vérification du formulaire
             VERIFICATION();
-        }else if (response.status===401){
+        } else if (response.status === 401){
+            // Affiche une alerte en cas de session expirée ou invalide
             alert('Session expirée ou invalide');
-        }else{
+        } else {
+            // Affiche une alerte en cas d'erreur technique inconnue
             alert('Erreur technique inconnue');
         }
-        })
-    
+    });
 }
 
 
 
-//Verification formulaire complet
+// Fonction de vérification du formulaire complet
 const VERIFICATION = function (e) {
-    if(INPUT_PICTURE.value != "" && CATEGORIES_SELECT.value !=0 && TITLE_NEW_PHOTO.value!=""){
-        BUTTON_SUBMIT.style.backgroundColor="#1D6154";
-        BUTTON_SUBMIT.style.cursor="pointer";
-        BUTTON_SUBMIT.addEventListener("click",UPLOAD_WORK);
-
-    }else{
-        BUTTON_SUBMIT.style.backgroundColor="#A7A7A7";
-        BUTTON_SUBMIT.style.cursor="default";
+    // Vérifie si l'image, la catégorie et le titre de la nouvelle photo sont renseignés
+    if(INPUT_PICTURE.value != "" && CATEGORIES_SELECT.value != 0 && TITLE_NEW_PHOTO.value != ""){
+        // Met à jour le style du bouton de soumission lorsque le formulaire est complet
+        BUTTON_SUBMIT.style.backgroundColor = "#1D6154";
+        BUTTON_SUBMIT.style.cursor = "pointer";
+        // Ajoute un écouteur d'événement pour la soumission du travail lorsque le formulaire est complet
+        BUTTON_SUBMIT.addEventListener("click", UPLOAD_WORK);
+    } else {
+        // Met à jour le style du bouton de soumission lorsque le formulaire est incomplet
+        BUTTON_SUBMIT.style.backgroundColor = "#A7A7A7";
+        BUTTON_SUBMIT.style.cursor = "default";
+        // Supprime l'écouteur d'événement pour la soumission du travail lorsque le formulaire est incomplet
         BUTTON_SUBMIT.removeEventListener("click", UPLOAD_WORK);
     }
 }
 
-INPUT_PICTURE.addEventListener("change",VERIFICATION);
-CATEGORIES_SELECT.addEventListener("change",VERIFICATION);
-TITLE_NEW_PHOTO.addEventListener("change",VERIFICATION);
+// Ajoute des écouteurs d'événement pour les changements dans les champs du formulaire
+INPUT_PICTURE.addEventListener("change", VERIFICATION);
+CATEGORIES_SELECT.addEventListener("change", VERIFICATION);
+TITLE_NEW_PHOTO.addEventListener("change", VERIFICATION);
 
-
-document.querySelectorAll('#ajout_projet').forEach(a=>{
-    a.addEventListener('click', OPEN_MODAL_NEW)
-})
+// Ajoute des écouteurs d'événement pour l'ouverture de la modal lors du clic sur les éléments avec l'ID 'ajout_projet'
+document.querySelectorAll('#ajout_projet').forEach(a => {
+    a.addEventListener('click', OPEN_MODAL_NEW);
+});
